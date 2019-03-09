d0d80ded1794ab0d03086224919431ccf2916431
Parveen Bhadoo <35890959+ParveenBhadooOfficial@users.noreply.github.com>
2019-03-09 12:06:00 +0530

    Update header.php

--- templates/neatblue/header.php
+++ templates/neatblue/header.php
@@ -6,7 +6,7 @@
 <meta name="robots" content="all">
 <meta name="description" content="Neatblue simple skins for rapidleech">
 <meta name="keywords" content="neatblue">
-<link rel="stylesheet" href="templates/neatblue/styles/rl_style_pm.css">
+<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/Th3-822/rapidleech/templates/neatblue/styles/rl_style_pm.css">
 <title><?php
 if (!isset($nn)) $nn = "\r\n";
 if (!isset($page_title)) {
@@ -20,15 +20,15 @@ if (!isset($page_title)) {
 var php_js_strings = [];
 php_js_strings[87] = " <?php echo lang(87); ?>";
 pic1= new Image(); 
-pic1.src="templates/neatblue/images/ajax-loading.gif";
+pic1.src="//cdn.jsdelivr.net/gh/Th3-822/rapidleech/templates/neatblue/images/ajax-loading.gif";
 /* ]]> */
 </script>
-<script src="classes/js.js"></script>
+<script src="//cdn.jsdelivr.net/gh/Th3-822/rapidleech/classes/js.js"></script>
 <?php
-if ($GLOBALS['options']['ajax_refresh']) { echo '<script src="classes/ajax_refresh.js"></script>'.$nn; }
-if ($GLOBALS['options']['flist_sort']) { echo '<script src="classes/sorttable.js"></script>'.$nn; }
+if ($GLOBALS['options']['ajax_refresh']) { echo '<script src="//cdn.jsdelivr.net/gh/Th3-822/rapidleech/classes/ajax_refresh.js"></script>'.$nn; }
+if ($GLOBALS['options']['flist_sort']) { echo '<script src="//cdn.jsdelivr.net/gh/Th3-822/rapidleech/classes/sorttable.js"></script>'.$nn; }
 ?>
 <script type="text/javascript">function toggle(b){var a=document.getElementById(b);if(a.style.display=="none"){a.style.display="block"}else{a.style.display="none"}};</script>
 </head>
 <body>
-<header><img src="templates/neatblue/images/logo_pm.gif" height="62" width="369" alt="rapidleech neatblue"></header><br />
+<header><img src="//cdn.jsdelivr.net/gh/Th3-822/rapidleech/templates/neatblue/images/logo_pm.gif" height="62" width="369" alt="rapidleech neatblue"></header><br />
